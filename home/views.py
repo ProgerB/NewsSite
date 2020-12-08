fadd2d65694f7d399bb6007b33634518dbc629b3
ProgerB <xurramovbahrom@mail.ru>
2020-12-08 19:22:24 +0500

    backup

--- home/views.py
+++ home/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render, HttpResponse
 
 def index(request):
     return render(request, 'index.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def post(request):
+    return render(request, 'post.html')
